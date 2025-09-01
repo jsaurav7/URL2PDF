27bd0e9230bf354adc671f0e590d11fa80393795
jsaurav7 <lynxrenb@gmail.com>
2025-09-01 14:29:10 +0530

    add ghostscript

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use chromiumoxide::page::ScreenshotParams;
 use chromiumoxide::{Browser, BrowserConfig};
 use futures::StreamExt;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
+use rayon::prelude::*;
 use serde::Deserialize;
 use serde_json::{json, Value};
 use std::cmp::PartialEq;
-use std::env;
 use std::fmt::Display;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 use std::time::Duration;
+use std::{env, fs, io};
 use uuid::Uuid;
+
+const GS: &str = "/opt/gs";
+// const GS: &str = "gswin64c";
 
 #[derive(PartialEq, Eq, Deserialize, Clone)]
 enum CaptureType {
@@ -53,9 +61,157 @@ async fn func(event: LambdaEvent<Value>) -> Result<Value, Box<dyn std::error::Er
 
     let data = capture(&event.url, event.capture_type.clone()).await?;
 
-    let url = upload(data, &bucket, event.capture_type).await?;
+    let url = if event.capture_type == CaptureType::PDF {
+        upload(split_compress_pdf(data).await?, &bucket, event.capture_type).await?
+    } else {
+        upload(data, &bucket, event.capture_type).await?
+    };
 
     Ok(json!({ "url": url }))
+}
+
+async fn split_compress_pdf(data: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let file_path = "/tmp/output.pdf";
+
+    let mut file = File::create(file_path)?;
+    file.write_all(&data)?;
+
+    let cores = num_cpus::get();
+    let page_count = page_count(file_path);
+
+    let compressed_chunk: Vec<_> =
+        split_pdf(file_path, page_count, (page_count + cores - 1) / cores)
+            .await?
+            .par_iter()
+            .map(|path| compress_pdf(path).unwrap().to_string_lossy().into_owned())
+            .collect();
+
+    merge_pdfs(&compressed_chunk, file_path).unwrap();
+
+    Ok(fs::read(file_path)?)
+}
+
+fn merge_pdfs(input_files: &[String], output_file: &str) -> io::Result<()> {
+    let files = format!("-sOutputFile={}", output_file);
+    let mut args = vec!["-dBATCH", "-dNOPAUSE", "-q", "-sDEVICE=pdfwrite", &files];
+
+    for file in input_files {
+        args.push(file);
+    }
+
+    let status = Command::new(GS).args(&args).status()?;
+
+    if status.success() {
+        println!("Merged PDFs into {}", output_file);
+        Ok(())
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Ghostscript merge failed",
+        ))
+    }
+}
+
+fn compress_pdf(file_path: &PathBuf) -> io::Result<PathBuf> {
+    let compressed_dir = Path::new("/tmp/compressed_chunks");
+
+    if !compressed_dir.exists() {
+        fs::create_dir_all(compressed_dir)?;
+    }
+
+    let compressed_file = compressed_dir.join(file_path);
+
+    let status = Command::new(GS)
+        .args([
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dPDFSETTINGS=/ebook",
+            "-dNOPAUSE",
+            "-dQUIET",
+            "-dBATCH",
+            &format!("-sOutputFile={}", compressed_file.to_string_lossy()),
+            file_path.to_str().unwrap(),
+        ])
+        .status()?;
+
+    if status.success() {
+        Ok(compressed_file)
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Ghostscript compression failed",
+        ))
+    }
+}
+
+async fn split_pdf(file_path: &str, pages: usize, chunk: usize) -> io::Result<Vec<PathBuf>> {
+    let out_dir = Path::new("/tmp/temp_chunks");
+    if !out_dir.exists() {
+        fs::create_dir_all(out_dir)?;
+    }
+
+    let jobs: Vec<(usize, usize, PathBuf)> = (1..=pages)
+        .step_by(chunk)
+        .map(|start| {
+            let end = usize::min(start + chunk - 1, pages);
+            let out_file = out_dir.join(format!("chunk_{}-{}.pdf", start, end));
+
+            (start, end, out_file)
+        })
+        .collect();
+
+    let results: Vec<_> = jobs
+        .par_iter()
+        .map(|(start, end, out_file)| {
+            let status = Command::new(GS)
+                .args([
+                    "-sDEVICE=pdfwrite",
+                    "-dNOPAUSE",
+                    "-dBATCH",
+                    "-dSAFER",
+                    "-dQUIET",
+                    &format!("-dFirstPage={}", start),
+                    &format!("-dLastPage={}", end),
+                    &format!("-sOutputFile={}", out_file.to_string_lossy()),
+                    file_path,
+                ])
+                .status();
+
+            match status {
+                Ok(s) if s.success() => Ok(out_file.clone()),
+                Ok(_) => Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    format!("Ghostscript failed for pages {}-{}", start, end),
+                )),
+                Err(e) => Err(e),
+            }
+        })
+        .collect();
+
+    let mut chunks = Vec::new();
+
+    for r in results {
+        chunks.push(r?);
+    }
+
+    Ok(chunks)
+}
+
+fn page_count(filepath: &str) -> usize {
+    let output = Command::new(GS)
+        .args([
+            "-q",
+            "-dNOSAFER",
+            "-dNODISPLAY",
+            "-c",
+            &format!("({}) (r) file runpdfbegin pdfpagecount = quit", filepath),
+        ])
+        .output()
+        .unwrap();
+
+    let page_count_str = String::from_utf8_lossy(&output.stdout);
+
+    page_count_str.trim().parse().unwrap_or(0)
 }
 
 async fn upload(
@@ -95,7 +251,6 @@ async fn capture(
     let (browser, mut handler) = Browser::launch(
         BrowserConfig::builder()
             .chrome_executable("/opt/chromium")
-            // .no_sandbox()
             .args([
                 "--allow-pre-commit-input",
                 "--disable-background-networking",
@@ -149,7 +304,7 @@ async fn capture(
             ])
             .build()?,
     )
-    .await?;
+        .await?;
 
     tokio::spawn(async move { while let Some(_) = handler.next().await {} });
 
